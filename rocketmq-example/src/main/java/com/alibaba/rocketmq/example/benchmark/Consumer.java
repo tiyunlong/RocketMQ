05353fabbed5b0270ce83d25d7f3bb4faafe0d3b
ti <ti@localhost>
2016-02-17 14:53:54 +0800

    test

--- rocketmq-example/src/main/java/com/alibaba/rocketmq/example/benchmark/Consumer.java
+++ rocketmq-example/src/main/java/com/alibaba/rocketmq/example/benchmark/Consumer.java
@@ -28,165 +28,159 @@ import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently
 import com.alibaba.rocketmq.client.exception.MQClientException;
 import com.alibaba.rocketmq.common.message.MessageExt;
 
-
 /**
  * 性能测试，订阅消息
  */
 public class Consumer {
 
-    public static void main(String[] args) throws MQClientException {
-        final StatsBenchmarkConsumer statsBenchmarkConsumer = new StatsBenchmarkConsumer();
+	public static void main(String[] args) throws MQClientException {
+		final StatsBenchmarkConsumer statsBenchmarkConsumer = new StatsBenchmarkConsumer();
 
-        final Timer timer = new Timer("BenchmarkTimerThread", true);
+		final Timer timer = new Timer("BenchmarkTimerThread", true);
 
-        final LinkedList<Long[]> snapshotList = new LinkedList<Long[]>();
+		final LinkedList<Long[]> snapshotList = new LinkedList<Long[]>();
 
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                snapshotList.addLast(statsBenchmarkConsumer.createSnapshot());
-                if (snapshotList.size() > 10) {
-                    snapshotList.removeFirst();
-                }
-            }
-        }, 1000, 1000);
+		timer.scheduleAtFixedRate(new TimerTask() {
 
-        timer.scheduleAtFixedRate(new TimerTask() {
-            private void printStats() {
-                if (snapshotList.size() >= 10) {
-                    Long[] begin = snapshotList.getFirst();
-                    Long[] end = snapshotList.getLast();
+			@Override
+			public void run() {
+				snapshotList.addLast(statsBenchmarkConsumer.createSnapshot());
+				if (snapshotList.size() > 10) {
+					snapshotList.removeFirst();
+				}
+			}
+		}, 1000, 1000);
 
-                    final long consumeTps =
-                            (long) (((end[1] - begin[1]) / (double) (end[0] - begin[0])) * 1000L);
-                    final double averageB2CRT = ((end[2] - begin[2]) / (double) (end[1] - begin[1]));
-                    final double averageS2CRT = ((end[3] - begin[3]) / (double) (end[1] - begin[1]));
+		timer.scheduleAtFixedRate(new TimerTask() {
 
-                    System.out.printf(
-                        "Consume TPS: %d Average(B2C) RT: %7.3f Average(S2C) RT: %7.3f MAX(B2C) RT: %d MAX(S2C) RT: %d\n"//
-                        , consumeTps//
-                        , averageB2CRT//
-                        , averageS2CRT//
-                        , end[4]//
-                        , end[5]//
-                        );
-                }
-            }
+			private void printStats() {
+				if (snapshotList.size() >= 10) {
+					Long[] begin = snapshotList.getFirst();
+					Long[] end = snapshotList.getLast();
 
+					final long consumeTps = (long) (((end[1] - begin[1]) / (double) (end[0] - begin[0])) * 1000L);
+					final double averageB2CRT = ((end[2] - begin[2]) / (double) (end[1] - begin[1]));
+					final double averageS2CRT = ((end[3] - begin[3]) / (double) (end[1] - begin[1]));
 
-            @Override
-            public void run() {
-                try {
-                    this.printStats();
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 10000, 10000);
+					System.out.printf(
+					        "Consume TPS: %d Average(B2C) RT: %7.3f Average(S2C) RT: %7.3f MAX(B2C) RT: %d MAX(S2C) RT: %d\n"//
+					        , consumeTps//
+					        , averageB2CRT//
+					        , averageS2CRT//
+					        , end[4]//
+					        , end[5]//
+					        );
+				}
+			}
 
-        DefaultMQPushConsumer consumer =
-                new DefaultMQPushConsumer("benchmark_consumer_"
-                        + Long.toString(System.currentTimeMillis() % 100));
-        consumer.setInstanceName(Long.toString(System.currentTimeMillis()));
+			@Override
+			public void run() {
+				try {
+					this.printStats();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}, 10000, 10000);
 
-        consumer.subscribe("BenchmarkTest", "*");
+		DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("benchmark_consumer_"
+		        + Long.toString(System.currentTimeMillis() % 100));
+		consumer.setInstanceName(Long.toString(System.currentTimeMillis()));
 
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
-            @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
-                    ConsumeConcurrentlyContext context) {
-                MessageExt msg = msgs.get(0);
-                long now = System.currentTimeMillis();
+		consumer.subscribe("BenchmarkTest", "*");
 
-                // 1
-                statsBenchmarkConsumer.getReceiveMessageTotalCount().incrementAndGet();
+		consumer.registerMessageListener(new MessageListenerConcurrently() {
 
-                // 2
-                long born2ConsumerRT = now - msg.getBornTimestamp();
-                statsBenchmarkConsumer.getBorn2ConsumerTotalRT().addAndGet(born2ConsumerRT);
+			@Override
+			public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+				MessageExt msg = msgs.get(0);
+				long now = System.currentTimeMillis();
 
-                // 3
-                long store2ConsumerRT = now - msg.getStoreTimestamp();
-                statsBenchmarkConsumer.getStore2ConsumerTotalRT().addAndGet(store2ConsumerRT);
+				// 1
+				statsBenchmarkConsumer.getReceiveMessageTotalCount().incrementAndGet();
 
-                // 4
-                compareAndSetMax(statsBenchmarkConsumer.getBorn2ConsumerMaxRT(), born2ConsumerRT);
+				// 2
+				long born2ConsumerRT = now - msg.getBornTimestamp();
+				statsBenchmarkConsumer.getBorn2ConsumerTotalRT().addAndGet(born2ConsumerRT);
 
-                // 5
-                compareAndSetMax(statsBenchmarkConsumer.getStore2ConsumerMaxRT(), store2ConsumerRT);
+				// 3
+				long store2ConsumerRT = now - msg.getStoreTimestamp();
+				statsBenchmarkConsumer.getStore2ConsumerTotalRT().addAndGet(store2ConsumerRT);
 
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-            }
-        });
+				// 4
+				compareAndSetMax(statsBenchmarkConsumer.getBorn2ConsumerMaxRT(), born2ConsumerRT);
 
-        consumer.start();
+				// 5
+				compareAndSetMax(statsBenchmarkConsumer.getStore2ConsumerMaxRT(), store2ConsumerRT);
 
-        System.out.println("Consumer Started.");
-    }
+				return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+			}
+		});
 
+		consumer.start();
 
-    public static void compareAndSetMax(final AtomicLong target, final long value) {
-        long prev = target.get();
-        while (value > prev) {
-            boolean updated = target.compareAndSet(prev, value);
-            if (updated)
-                break;
+		System.out.println("Consumer Started.");
+	}
 
-            prev = target.get();
-        }
-    }
+	public static void compareAndSetMax(final AtomicLong target, final long value) {
+		long prev = target.get();
+		while (value > prev) {
+			boolean updated = target.compareAndSet(prev, value);
+			if (updated)
+				break;
+
+			prev = target.get();
+		}
+	}
 }
 
-
 class StatsBenchmarkConsumer {
-    // 1
-    private final AtomicLong receiveMessageTotalCount = new AtomicLong(0L);
-    // 2
-    private final AtomicLong born2ConsumerTotalRT = new AtomicLong(0L);
-    // 3
-    private final AtomicLong store2ConsumerTotalRT = new AtomicLong(0L);
-    // 4
-    private final AtomicLong born2ConsumerMaxRT = new AtomicLong(0L);
-    // 5
-    private final AtomicLong store2ConsumerMaxRT = new AtomicLong(0L);
 
+	// 1
+	private final AtomicLong receiveMessageTotalCount = new AtomicLong(0L);
 
-    public Long[] createSnapshot() {
-        Long[] snap = new Long[] {//
-                System.currentTimeMillis(),//
-                        this.receiveMessageTotalCount.get(),//
-                        this.born2ConsumerTotalRT.get(),//
-                        this.store2ConsumerTotalRT.get(),//
-                        this.born2ConsumerMaxRT.get(),//
-                        this.store2ConsumerMaxRT.get(), //
-                };
+	// 2
+	private final AtomicLong born2ConsumerTotalRT = new AtomicLong(0L);
 
-        return snap;
-    }
+	// 3
+	private final AtomicLong store2ConsumerTotalRT = new AtomicLong(0L);
 
+	// 4
+	private final AtomicLong born2ConsumerMaxRT = new AtomicLong(0L);
 
-    public AtomicLong getReceiveMessageTotalCount() {
-        return receiveMessageTotalCount;
-    }
+	// 5
+	private final AtomicLong store2ConsumerMaxRT = new AtomicLong(0L);
 
+	public Long[] createSnapshot() {
+		Long[] snap = new Long[] {//
+		System.currentTimeMillis(),//
+		        this.receiveMessageTotalCount.get(),//
+		        this.born2ConsumerTotalRT.get(),//
+		        this.store2ConsumerTotalRT.get(),//
+		        this.born2ConsumerMaxRT.get(),//
+		        this.store2ConsumerMaxRT.get(), //
+		};
 
-    public AtomicLong getBorn2ConsumerTotalRT() {
-        return born2ConsumerTotalRT;
-    }
+		return snap;
+	}
 
+	public AtomicLong getReceiveMessageTotalCount() {
+		return receiveMessageTotalCount;
+	}
 
-    public AtomicLong getStore2ConsumerTotalRT() {
-        return store2ConsumerTotalRT;
-    }
+	public AtomicLong getBorn2ConsumerTotalRT() {
+		return born2ConsumerTotalRT;
+	}
 
+	public AtomicLong getStore2ConsumerTotalRT() {
+		return store2ConsumerTotalRT;
+	}
 
-    public AtomicLong getBorn2ConsumerMaxRT() {
-        return born2ConsumerMaxRT;
-    }
+	public AtomicLong getBorn2ConsumerMaxRT() {
+		return born2ConsumerMaxRT;
+	}
 
-
-    public AtomicLong getStore2ConsumerMaxRT() {
-        return store2ConsumerMaxRT;
-    }
+	public AtomicLong getStore2ConsumerMaxRT() {
+		return store2ConsumerMaxRT;
+	}
 }
